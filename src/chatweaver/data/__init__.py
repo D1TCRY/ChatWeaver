26192d76507ab54be3d531a3d5220a6ba1ff5dcf
Diego Cecchelani <134800819+D1TCRY@users.noreply.github.com>
2026-05-03 19:40:58 +0200

    Delete src directory

--- src/chatweaver/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import ChatWeaverModelNames
-from .system_rules import ChatWeaverSystemRules, Formatting, Language
-
-__all__ = ["ChatWeaverModelNames", "ChatWeaverSystemRules", "Formatting", "Language"]
